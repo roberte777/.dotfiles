2c73e74cfbd36c48de1b99fd65dd9f02e47805d5
roberte777 <robewilkes@yahoo.com>
2022-04-30 21:07:13 -0500

    added auto install scripts and kitty setup

---
+++ nvim_install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
+cd $HOME
+mkdir install_repos
+cd install_repos
+git clone https://github.com/neovim/neovim
+cd neovim
+git checkout v0.7.0
+make CMAKE_BUILD_TYPE=Release
+sudo make install
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
