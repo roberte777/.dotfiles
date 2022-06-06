3f8dc5dd7a712ac7a3ed20fcc7b7cc0519262d44
Ethan Wilkes <ethan@Ethans-MacBook-Pro.local>
2022-06-06 15:51:21 -0500

    added ability to navigate with mouse

--- nvim/.config/nvim/lua/roberte777/_barbar.lua
+++ nvim/.config/nvim/lua/roberte777/_barbar.lua
@@ -80,7 +80,7 @@ require'bufferline'.setup {
   -- Configure icons on the bufferline.
   icon_separator_active = '▎',
   icon_separator_inactive = '▎',
-  icon_close_tab = '',
+  icon_close_tab = 'X',
   icon_close_tab_modified = '●',
   icon_pinned = '車',
 
