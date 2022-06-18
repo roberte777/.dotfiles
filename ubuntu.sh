89d768fb374d33d5b086a1e31e3cbefabbb1721d
roberte777 <robewilkes@yahoo.com>
2022-06-18 00:06:08 -0500

    added fonts to settings

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env zsh
+echo "[-] Download fonts [-]"
+echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Monoki.zip"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Mononoki.zip
+unzip Mononoki.zip -d ~/.local/share/fonts
+fc-cache -fv
+echo "done!"
 if [[ -z $STOW_FOLDERS ]]; then
     STOW_FOLDERS="nvim, kitty, awesome"
 fi
@@ -8,3 +14,4 @@ if [[ -z $DOTFILES ]]; then
 fi
 
 STOW_FOLDERS=$STOW_FOLDERS DOTFILES=$DOTFILES $DOTFILES/install.sh
+
