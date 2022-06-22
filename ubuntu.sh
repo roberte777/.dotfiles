20f82b8b11a3b5a91ce8db82d236596ddb35a346
roberte777 <robewilkes@yahoo.com>
2022-06-21 21:58:01 -0500

    even more config in ubuntu install

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,6 +2,8 @@
 #install things I am using right off the bat
 sudo apt install stow
 sudo apt install zsh
+# install oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 #set default terminal to zsh
 chsh -s $(which zsh)
 sudo apt install curl
@@ -38,3 +40,16 @@ fi
 
 STOW_FOLDERS=$STOW_FOLDERS DOTFILES=$DOTFILES $DOTFILES/install.sh
 
+#install rofi, the things that lets you run stuff
+sudo apt install rofi
+sudo apt install libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libpcre3-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev meson
+cd
+mkdir install_repos
+cd install_repos
+git clone https://github.com/yshui/picom.git
+cd picom
+git submodule update --init --recursive
+meson --buildtype=release . build
+ninja -C build
+sudo ninja -C build install
+sudo apt install awesome
