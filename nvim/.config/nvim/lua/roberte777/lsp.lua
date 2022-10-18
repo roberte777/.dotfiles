9bceb29200ea254f056a4145ac1b3b66f2060117
roberte777 <robewilkes@yahoo.com>
2022-10-18 11:34:26 -0500

    update

--- nvim/.config/nvim/lua/roberte777/lsp.lua
+++ nvim/.config/nvim/lua/roberte777/lsp.lua
@@ -71,7 +71,7 @@ cmp.setup({
 
 local function config(_config)
 	return vim.tbl_deep_extend("force", {
-		capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+		capabilities = require("cmp_nvim_lsp").default_capabilities(),
 		on_attach = function()
             nnoremap("<leader>gtd",function()
                 require("telescope.builtin").lsp_type_definitions()
