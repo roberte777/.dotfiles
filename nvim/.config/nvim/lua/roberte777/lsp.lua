aa12f0debcf7a5101d50c2e67bf0a1a72d2c2a4d
roberte777 <robewilkes@yahoo.com>
2023-03-02 16:11:28 -0600

    added descriptions for keymaps)

--- nvim/.config/nvim/lua/roberte777/lsp.lua
+++ nvim/.config/nvim/lua/roberte777/lsp.lua
@@ -29,40 +29,46 @@ lsp.setup_nvim_cmp({
 	},
 	mapping = cmp_mappings,
 })
+local function add_to_table(add_to, to_add)
+	for k, v in pairs(to_add) do
+		add_to[k] = v
+	end
+	return add_to -- for convenience (chaining)
+end
 
 lsp.on_attach(function(client, bufnr)
 	local opts = { buffer = bufnr, remap = false }
 
 	vim.keymap.set("n", "gd", function()
 		vim.lsp.buf.definition()
-	end, opts)
+	end, add_to_table(opts, { desc = "Go to definition" }))
 	vim.keymap.set("n", "gtd", function()
 		vim.lsp.buf.type_definition()
-	end, opts)
+	end, add_to_table(opts, { desc = "Go to type definition" }))
 	vim.keymap.set("n", "K", function()
 		vim.lsp.buf.hover()
-	end, opts)
+	end, add_to_table(opts, { desc = "Hover" }))
 	vim.keymap.set("n", "<leader>vws", function()
 		vim.lsp.buf.workspace_symbol()
-	end, opts)
+	end, add_to_table(opts, { desc = "Workspace symbols" }))
 	vim.keymap.set("n", "<leader>vd", function()
 		vim.diagnostic.open_float()
-	end, opts)
+	end, add_to_table(opts, { desc = "Open diagnostics" }))
 	vim.keymap.set("n", "[d", function()
 		vim.diagnostic.goto_next()
-	end, opts)
+	end, add_to_table(opts, { desc = "Go to next diagnostic" }))
 	vim.keymap.set("n", "]d", function()
 		vim.diagnostic.goto_prev()
-	end, opts)
+	end, add_to_table(opts, { desc = "Go to previous diagnostic" }))
 	vim.keymap.set("n", "<leader>vca", function()
 		vim.lsp.buf.code_action()
-	end, opts)
+	end, add_to_table(opts, { desc = "Code actions" }))
 	vim.keymap.set("n", "<leader>vrr", function()
 		vim.lsp.buf.references()
-	end, opts)
+	end, add_to_table(opts, { desc = "References" }))
 	vim.keymap.set("n", "<leader>vrn", function()
 		vim.lsp.buf.rename()
-	end, opts)
+	end, add_to_table(opts, { desc = "Rename" }))
 	-- vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
