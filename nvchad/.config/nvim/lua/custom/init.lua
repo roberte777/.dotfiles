5749bc1dad4eb04d443cac4143885a33f9a56f42
roberte777 <robewilkes@yahoo.com>
2023-04-23 01:52:03 -0500

    made it so copilot is tab:

--- nvchad/.config/nvim/lua/custom/init.lua
+++ nvchad/.config/nvim/lua/custom/init.lua
@@ -1,3 +1,1 @@
 -- PUT NEOVIM OPTIONS HERE. OPTIONS FOR PLUGINS AND NVCHAD GO IN CHADRC
-vim.g.copilot_assume_mapped = true
-vim.g.copilot_no_tab_map = true
--- nvchad/.config/nvim/lua/custom/mappings.lua
+++ nvchad/.config/nvim/lua/custom/mappings.lua
@@ -7,8 +7,6 @@ M.disabled = {
 M.abc = {
 	i = {
 		["<C-c>"] = { "<ESC>", "escape insert mode", opts = { nowait = true } },
-		-- copiloat accept suggestion
-		["<C-l>"] = { 'copilot#Accept("<CR>")', opts = { silent = true, expr = true } },
 	},
 }
 
