8e8c477a5f0628c6c23a3cd606bf47a2803ad299
roberte777 <robewilkes@yahoo.com>
2023-06-28 00:39:13 -0500

    updated nerd fonts

--- fonts.sh
+++ fonts.sh
@@ -1,7 +1,7 @@
 
 echo "[-] Download fonts [-]"
-echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/JetBrainsMono.zip"
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/JetBrainsMono.zip
+echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
 unzip JetBrainsMono.zip -d ~/.local/share/fonts
 fc-cache -fv
 rm JetBrainsMono.zip
