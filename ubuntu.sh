25672ca3ae42cb5f53562c311aa6699b26d20cb3
roberte777 <roberte777@Ubuntu.myguest.virtualbox.org>
2023-07-02 16:07:34 -0500

    added setup for alacritty, changed a few things from the config

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,6 +2,8 @@
 
 #install things I am using right off the bat
 sudo apt install stow
+#install grep for telescope nvim
+sudo apt-get install ripgrep
 sudo apt install zsh
 sudo apt install curl
 sudo apt install pavucontrol
@@ -12,6 +14,8 @@ chsh -s $(which zsh)
 # stow config files
 chmod a+x ./stow.sh
 ./stow.sh
+
+### Starting desktop installs
 #install kitty terminal
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 #set up bin folder for kitty
@@ -20,31 +24,27 @@ cd .local
 mkdir bin
 cd
 #kitty setup for linux
-ln -s ~/.local/kitty.app/bin/kitty ~/.local/bin/
+ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
 cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
 cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
 sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
 sed -i "s|Exec=kitty|Exec=/home/$USER/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
 #downloading fonts
 echo "[-] Download fonts [-]"
-echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Monoki.zip"
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Mononoki.zip
-unzip Mononoki.zip -d ~/.local/share/fonts
-fc-cache -fv
-rm Mononoki.zip
-#install grep for telescope nvim
-sudo apt-get install ripgrep
+./fonts.sh
 
 #install rofi, the things that lets you run stuff
 sudo apt install rofi
-sudo apt install libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libpcre3-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev meson
-cd
-mkdir install_repos
-cd install_repos
-git clone https://github.com/yshui/picom.git
-cd picom
-git submodule update --init --recursive
-meson --buildtype=release . build
-ninja -C build
-sudo ninja -C build install
+### Picom. Not sure if I need to build from source or not, so leaving this here for now
+# sudo apt install libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libpcre3-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev meson
+# cd
+# mkdir install_repos
+# cd install_repos
+# git clone https://github.com/yshui/picom.git
+# cd picom
+# git submodule update --init --recursive
+# meson --buildtype=release . build
+# ninja -C build
+# sudo ninja -C build install
+sudo apt install picom
 sudo apt install awesome
