7123b5df25f3d4a5536806266eb750f490b850d5
roberte777 <robewilkes@yahoo.com>
2023-07-14 13:34:22 -0500

    added tools to lazyvim for all the languages I typically use

--- lazyvim/.config/nvim/lua/config/lazy.lua
+++ lazyvim/.config/nvim/lua/config/lazy.lua
@@ -14,6 +14,14 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.lang.clangd" },
+    { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.lang.python" },
+
     -- import/override with your plugins
     { import = "plugins" },
   },
--- lazyvim/.config/nvim/lua/plugins/lsp.lua
+++ lazyvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,13 +1,14 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        -- pyright will be automatically installed with mason and loaded with lspconfig
-        pyright = {},
-        rust_analyzer = {},
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      rust_analyzer = {
+        keys = {
+          { "K", "<cmd>RustHoverActions<cr>", desc = "Hover Actions (Rust)" },
+          { "<leader>cR", "<cmd>RustCodeAction<cr>", desc = "Code Action (Rust)" },
+          { "<leader>dr", "<cmd>RustDebuggables<cr>", desc = "Run Debuggables (Rust)" },
+          { "<leader>cx", "<cmd>RustRunnables<cr>", desc = "Rust Runnables" },
+        },
       },
     },
   },
