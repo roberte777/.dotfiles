d32acc2cb6d368377b1f398859a27919f0f6ad3b
roberte777 <robewilkes@yahoo.com>
2023-07-14 12:28:13 -0500

    added lazyvim

---
+++ lazyvim/.config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "hrsh7th/cmp-emoji" },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      local defaults = require("cmp.config.default")
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" } }))
+      opts.preselect = "None"
+      opts.completion.completeopt = "menu,menuone,noinsert,noselect"
+      opts.mapping = cmp.mapping.preset.insert({
+        ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<S-CR>"] = cmp.mapping.confirm({
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = true,
+        }),
+      })
+    end,
+  },
+}
---
+++ lazyvim/.config/nvim/lua/plugins/colors.lua
@@ -0,0 +1,12 @@
+return {
+  { "ellisonleao/gruvbox.nvim" },
+  {
+    "https://github.com/sainnhe/gruvbox-material",
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox-material",
+    },
+  },
+}
