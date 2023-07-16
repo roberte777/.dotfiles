5c89b6bc2907163c78e72b9513e646eaa674a041
roberte777 <robewilkes@yahoo.com>
2023-07-15 21:37:16 -0700

    added config examples that I may like

--- lazyvim/.config/nvim/lua/plugins/colors.lua
+++ lazyvim/.config/nvim/lua/plugins/colors.lua
@@ -2,6 +2,10 @@ return {
   { "ellisonleao/gruvbox.nvim" },
   {
     "https://github.com/sainnhe/gruvbox-material",
+    config = function()
+      -- vim.g.gruvbox_material_background = "hard"
+      -- vim.g.gruvbox_material_foreground = "mix"
+    end,
   },
   {
     "LazyVim/LazyVim",
