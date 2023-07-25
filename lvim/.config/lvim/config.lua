897d46691700c06e5a5e6b846b5785b4a9c7c2f3
ewilkes <ethan.wilkes@torchtechnologies.com>
2023-07-25 17:42:06 -0500

    edited a few settings

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -2,11 +2,12 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
---
-lvim.colorscheme = "dracula"
+
 lvim.plugins = {
   {
     "Mofiqul/dracula.nvim",
-    event = "Colorscheme"
+    lazy = false,
+    priority = 1000,
   }
 }
+lvim.colorscheme = "dracula"
