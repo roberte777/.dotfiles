bcd1039a9784e5eaa6009c90567a1ac2e103a4a3
ewilkes <ethan.wilkes@torchtechnologies.com>
2023-07-27 09:33:08 -0500

    updated a few settings

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -11,3 +11,9 @@ lvim.plugins = {
   }
 }
 lvim.colorscheme = "dracula"
+lvim.format_on_save.enabled = true
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  { name = "black" },
+  { name = "rustfmt" }
+}
