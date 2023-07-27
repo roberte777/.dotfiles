3d00d1f72d9025ffb5089ac2e5bd6a978e0ce3cf
ewilkes <ethan.wilkes@torchtechnologies.com>
2023-07-27 10:03:39 -0500

    removed rustfmt from null-ls cause that was causing an error

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -15,6 +15,5 @@ lvim.format_on_save.enabled = true
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
   { name = "black" },
-  { name = "rustfmt" }
 }
 lvim.transparent_window = true
