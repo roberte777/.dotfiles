d7f0297d259f53cfcf75bc59023b96de90be6f44
roberte777 <robewilkes@yahoo.com>
2023-07-29 18:14:29 -0700

    added a few plugins that I liked from LazyVim

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -8,9 +8,66 @@ lvim.plugins = {
     "Mofiqul/dracula.nvim",
     lazy = false,
     priority = 1000,
-  }
+  },
+  { "sainnhe/gruvbox-material", lazy = false, priority = 1000 },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    vscode = true,
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+      { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+      { "r", mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+      { "R", mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      {
+        "<c-s>",
+        mode = { "c" },
+        function() require("flash").toggle() end,
+        desc =
+        "Toggle Flash Search"
+      },
+    },
+  },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    }
+  },
+  {
+    "echasnovski/mini.nvim",
+    opts = {
+      surround = {
+        mappings = {
+          add = "gza",            -- Add surrounding in Normal and Visual modes
+          delete = "gzd",         -- Delete surrounding
+          find = "gzf",           -- Find surrounding (to the right)
+          find_left = "gzF",      -- Find surrounding (to the left)
+          highlight = "gzh",      -- Highlight surrounding
+          replace = "gzr",        -- Replace surrounding
+          update_n_lines = "gzn", -- Update `n_lines`
+        },
+      }
+    },
+    config = function(_, opts)
+      require("mini.surround").setup(opts.surround)
+    end
+
+  },
 }
-lvim.colorscheme = "dracula"
+lvim.colorscheme = "gruvbox-material"
 lvim.format_on_save.enabled = true
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
