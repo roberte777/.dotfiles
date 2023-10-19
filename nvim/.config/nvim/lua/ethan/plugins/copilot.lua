12d124f525d56ec2f66a851bad50d98dc6bcabad
ewilkes <ethan.wilkes@torchtechnologies.com>
2023-10-19 14:08:26 -0500

    start copilot on insert enter

--- nvim/.config/nvim/lua/ethan/plugins/copilot.lua
+++ nvim/.config/nvim/lua/ethan/plugins/copilot.lua
@@ -3,6 +3,7 @@ return {
 		"zbirenbaum/copilot.lua",
 		cmd = "Copilot",
 		build = ":Copilot auth",
+		event = "InsertEnter",
 		config = function()
 			require("copilot").setup({ suggestion = { auto_trigger = true, keymap = { accept = "<C-l>" } } })
 		end,
