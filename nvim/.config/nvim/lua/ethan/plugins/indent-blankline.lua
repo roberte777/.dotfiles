ddbdfe3f76f66b921d4c3111cc5842f83e09d146
roberte777 <robewilkes@yahoo.com>
2024-02-17 01:33:39 -0600

    added new plugins

--- nvim/.config/nvim/lua/ethan/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/ethan/plugins/indent-blankline.lua
@@ -1,3 +1,12 @@
 return {
-	{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		opts = {
+			indent = {
+				char = "│",
+				tab_char = "│",
+			},
+		},
+	},
 }
--- nvim/.config/nvim/lua/ethan/plugins/mini.lua
+++ nvim/.config/nvim/lua/ethan/plugins/mini.lua
@@ -1,19 +1,33 @@
 return {
 	{
-		"echasnovski/mini.nvim",
+		"echasnovski/mini.pairs",
 		version = false,
 		config = function()
-			require("mini.indentscope").setup()
-			require("mini.pairs").setup()
+			require("mini.pairs").setup({})
 		end,
+	},
+	{
+		"echasnovski/mini.completion",
+		version = false,
+	},
+	{
+		"echasnovski/mini.indentscope",
+		version = false, -- wait till new 0.7.0 release to put it back on semver
+		opts = {
+			-- symbol = "▏",
+			symbol = "│",
+			options = { try_as_border = true },
+		},
 		init = function()
 			vim.api.nvim_create_autocmd("FileType", {
 				pattern = {
 					"help",
 					"alpha",
 					"dashboard",
+					"neo-tree",
 					"NvimTree",
 					"Trouble",
+					"trouble",
 					"lazy",
 					"mason",
 					"notify",
@@ -23,6 +37,82 @@ return {
 				callback = function()
 					vim.b.miniindentscope_disable = true
 				end,
+			})
+		end,
+	},
+	{
+		"echasnovski/mini.ai",
+		-- keys = {
+		--   { "a", mode = { "x", "o" } },
+		--   { "i", mode = { "x", "o" } },
+		-- },
+		event = "VeryLazy",
+		dependencies = {
+			"folke/which-key.nvim",
+			{
+				"nvim-treesitter/nvim-treesitter-textobjects",
+				init = function()
+					-- no need to load the plugin, since we only need its queries
+					require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
+				end,
+			},
+		},
+		opts = function()
+			local ai = require("mini.ai")
+			return {
+				n_lines = 500,
+				custom_textobjects = {
+					o = ai.gen_spec.treesitter({
+						a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+						i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+					}, {}),
+					f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
+					c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
+					t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" },
+				},
+			}
+		end,
+		config = function(_, opts)
+			require("mini.ai").setup(opts)
+			---@type table<string, string|table>
+			local i = {
+				[" "] = "Whitespace",
+				['"'] = 'Balanced "',
+				["'"] = "Balanced '",
+				["`"] = "Balanced `",
+				["("] = "Balanced (",
+				[")"] = "Balanced ) including white-space",
+				[">"] = "Balanced > including white-space",
+				["<lt>"] = "Balanced <",
+				["]"] = "Balanced ] including white-space",
+				["["] = "Balanced [",
+				["}"] = "Balanced } including white-space",
+				["{"] = "Balanced {",
+				["?"] = "User Prompt",
+				_ = "Underscore",
+				a = "Argument",
+				b = "Balanced ), ], }",
+				c = "Class",
+				f = "Function",
+				o = "Block, conditional, loop",
+				q = "Quote `, \", '",
+				t = "Tag",
+			}
+			local a = vim.deepcopy(i)
+			for k, v in pairs(a) do
+				a[k] = v:gsub(" including.*", "")
+			end
+
+			local ic = vim.deepcopy(i)
+			local ac = vim.deepcopy(a)
+			for key, name in pairs({ n = "Next", l = "Last" }) do
+				i[key] = vim.tbl_extend("force", { name = "Inside " .. name .. " textobject" }, ic)
+				a[key] = vim.tbl_extend("force", { name = "Around " .. name .. " textobject" }, ac)
+			end
+			require("which-key").register({
+				mode = { "o", "x" },
+				i = i,
+				a = a,
 			})
 		end,
 	},
