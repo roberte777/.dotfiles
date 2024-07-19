aa4e99caad22c51a141204cbbba76bef3259520a
ewilkes <ewilkes@torchtechnologies.com>
2024-07-19 13:28:04 -0500

    fix: fixed fonts

--- fonts.sh
+++ fonts.sh
@@ -1,7 +1,7 @@
 
 echo "[-] Download fonts [-]"
-echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip"
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
+echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip
 unzip JetBrainsMono.zip -d ~/.local/share/fonts
 fc-cache -fv
 rm JetBrainsMono.zip
