a071666b3f3a853e6b6e5be7c6d9c9abcfd1a131
roberte777 <rewilkes0041@gmail.com>
2024-07-27 23:53:33 +0000

    feat: fixed which key to v3

--- nvim/.config/nvim/lua/ethan/plugins/which-key.lua
+++ nvim/.config/nvim/lua/ethan/plugins/which-key.lua
@@ -7,17 +7,16 @@ return {
 			-- or leave it empty to use the default settings
 			-- refer to the configuration section below
 		},
-		config = function() -- This is the function that runs, AFTER loading
-			require("which-key").setup()
-
+		config = function(_, opts) -- This is the function that runs, AFTER loading
 			-- Document existing key chains
-			require("which-key").register({
-				["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
-				["<leader>c"] = { name = "[C]ode Actions", _ = "which_key_ignore" },
-				["<leader>h"] = { name = "[H]arpoon", _ = "which_key_ignore" },
-				["<leader>f"] = { name = "[F]ind", _ = "which_key_ignore" },
-				["<leader>u"] = { name = "Toggle", _ = "which_key_ignore" },
-				["<leader>x"] = { name = "Trouble", _ = "which_key_ignore" },
+			require("which-key").add({
+				{ "<leader>t", group = "Toggle" },
+				{ "<leader>c", group = "Code Actions" },
+				{ "<leader>x", group = "Trouble" },
+				{ "<leader>h", group = "Harpoon" },
+				{ "<leader>f", group = "Find" },
+				{ "<leader>g", group = "Git" },
+				{ "<leader>d", group = "Debug" },
 			})
 		end,
 	},
