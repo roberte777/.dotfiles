6797785319e0c13c0a4e7ad62e5465395880c441
roberte777 <rewilkes0041@gmail.com>
2024-10-22 17:52:51 -0500

    added hyprlock to arch install script

--- arch-apps.sh
+++ arch-apps.sh
@@ -1,3 +1,3 @@
 yay -S nwg-displays rofi-wayland hyprpaper \
     waybar pavucontrol starship plasma-polkit-agent \
-    swaync libnotify systemd openfortivpn
+    swaync libnotify systemd openfortivpn hyprlock
