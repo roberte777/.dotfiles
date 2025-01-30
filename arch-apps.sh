812bc3148646b969d155407129603953bfbba451
roberte777 <rewilkes0041@gmail.com>
2025-01-30 16:59:42 -0600

    hyprland changes

--- arch-apps.sh
+++ arch-apps.sh
@@ -1,3 +1,10 @@
 yay -S nwg-displays rofi-wayland hyprpaper \
     waybar pavucontrol starship plasma-polkit-agent \
-    swaync libnotify systemd openfortivpn hyprlock
+    swaync libnotify systemd openfortivpn hyprlock \
+    ags-hyprpanel-git
+
+# hyprland plugin
+hyprpm add https://github.com/Duckonaut/split-monitor-workspaces # Add the plugin repository
+hyprpm enable split-monitor-workspaces # Enable the plugin
+hyprpm reload # Reload the plugins
+hyprpm update
--- fonts.sh
+++ fonts.sh
@@ -1,7 +1,6 @@
-
 echo "[-] Download fonts [-]"
-echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip"
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip
+echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/JetBrainsMono.zip"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/JetBrainsMono.zip
 unzip JetBrainsMono.zip -d ~/.local/share/fonts
 fc-cache -fv
 rm JetBrainsMono.zip
