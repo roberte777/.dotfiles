1c1dedb516bb1632da04fad4602a3cf7a495cd97
ewilkes <ewilkes@torchtechnologies.com>
2025-06-16 11:51:09 -0500

    feat: added disable format command

--- nvim/.config/nvim/lua/ethan/plugins/lsp/conform.lua
+++ nvim/.config/nvim/lua/ethan/plugins/lsp/conform.lua
@@ -51,16 +51,35 @@ return {
 				c = { "clang-format" },
 				cpp = { "clang-format" },
 			},
-			format_on_save = {
-				-- These options will be passed to conform.format()
-				timeout_ms = 500,
-				lsp_fallback = false,
-			},
+			format_on_save = function(bufnr)
+				-- Disable with a global or buffer-local variable
+				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+					return
+				end
+				return { timeout_ms = 500, lsp_format = "fallback" }
+			end,
 		},
 		config = function(_, opts)
 			local util = require("conform.util")
 			-- util.add_formatter_args(require("conform.formatters.rustfmt"), { "--edition", "2021" })
 			require("conform").setup(opts)
+			vim.api.nvim_create_user_command("FormatDisable", function(args)
+				if args.bang then
+					-- FormatDisable! will disable formatting just for this buffer
+					vim.b.disable_autoformat = true
+				else
+					vim.g.disable_autoformat = true
+				end
+			end, {
+				desc = "Disable autoformat-on-save",
+				bang = true,
+			})
+			vim.api.nvim_create_user_command("FormatEnable", function()
+				vim.b.disable_autoformat = false
+				vim.g.disable_autoformat = false
+			end, {
+				desc = "Re-enable autoformat-on-save",
+			})
 		end,
 	},
 }
