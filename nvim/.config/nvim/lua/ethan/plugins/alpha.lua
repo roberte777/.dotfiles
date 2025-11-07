7ae7f4351bb027d8319a0cc99e841eeca2f75207
roberte777 <rewilkes0041@gmail.com>
2025-11-06 23:08:06 -0600

    few nvim updates

--- nvim/.config/nvim/lua/ethan/plugins/alpha.lua
+++
@@ -1,9 +0,0 @@
-return {}
--- return {
--- 	{
--- 		"goolord/alpha-nvim",
--- 		config = function()
--- 			require("alpha").setup(require("alpha.themes.dashboard").config)
--- 		end,
--- 	},
--- }
--- nvim/.config/nvim/lua/ethan/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/ethan/plugins/harpoon.lua
@@ -16,7 +16,7 @@ return {
 
         vim.keymap.set("n", "<leader>ha",
             function()
-                harpoon:list():append()
+                harpoon:list():add()
             end,
             { noremap = true, silent = true, desc = "Add file to harpoon menu" })
 
